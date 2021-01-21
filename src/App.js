d29d1c00c0ee8ac615e8bc7f24a16ce7c2e6306b
sbayrak <34121748+sbayrak@users.noreply.github.com>
2021-01-21 10:30:45 +0300

    netlify test 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
-
+const encode = (data) => {
+  return Object.keys(data)
+    .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+    .join('&');
+};
 function App() {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({
+        'form-name': 'contact',
+        name: name.text,
+        email: email.text,
+        message: message.text,
+      }),
+    })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((error) => alert(error));
+  };
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <form
+        onSubmit={handleSubmit}
+        data-netlify='true'
+        name='contact'
+        method='post'
+        style={{ display: 'flex', flexDirection: 'column' }}
+      >
+        <input type='hidden' name='form-name' value='contact' />
+
+        <label htmlFor='name'>Your Name: </label>
+        <input
+          type='text'
+          name='name'
+          value={name}
+          placeholder='Your Name'
+          onChange={(e) => setName(e.target.value)}
+        />
+
+        <label htmlFor='email'>Your Email: </label>
+        <input
+          type='email'
+          name='email'
+          value={email}
+          placeholder='you@youremail.com'
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <label htmlFor='message'>Message: </label>
+        <textarea
+          name='message'
+          value={message}
+          placeholder='What do you want to say?'
+          onChange={(e) => setMessage(e.target.value)}
+        />
+
+        <button type='submit'>Send Message</button>
+      </form>
     </div>
   );
 }
